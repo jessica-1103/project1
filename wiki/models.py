99340e8f667585ff908f9b20c5dc5a7fae655a23
Jessy <jessyrosalesornelas@gmail.com>
2020-08-01 20:59:16 -0500

    add models

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=50, default='')
+    descripcion = models.TextField(blank=True, default='')
+    autor = models.ForeignKey('Autor', on_delete=models.SET_NULL, null=True)
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=50, default='')
+    edad = models.IntegerField(blank=True)
+
